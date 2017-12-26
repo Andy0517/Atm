0c1ec04a495c5ad8eecb526d89f3d3a0905a23c4
Andy0517 <ad870517@gmail.com>
2017-12-26 12:01:59 +0800

    area

---
+++ app/src/main/java/com/lin/atm/AreaActivity.java
@@ -0,0 +1,52 @@
+package com.lin.atm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import static com.lin.atm.CityActivity.GO_KEELUNG;
+
+public class AreaActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    ListView area;
+    String[] data = null;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.data = 
+        switch(requestCode){
+            case GO_KEELUNG:
+                data = new String[]{"暖暖區","八堵區","中正區"};
+                ArrayAdapter adapter =new ArrayAdapter(this,android.R.layout.simple_list_item_1,AreaActivity.data);
+                area.setAdapter(adapter);
+                area.setOnItemClickListener(this);
+        }
+        if(position == 1){
+            data = new String[]{"永和區","新莊區","板橋區"};
+        }
+        if(position == 2){
+            data = new String[]{"大安區","士林區","信義區"};
+
+        }
+        ArrayAdapter adapter = new ArrayAdapter(AddrActivity.this,android.R.layout.simple_list_item_1,data);
+    }
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_area);
+        area = (ListView) findViewById(R.id.area);
+        String[] data = {"基隆市", "新北市", "台北市"};
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, data);
+        area.setAdapter(adapter);
+        area.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        }
+    }
+}
--- app/src/main/java/com/lin/atm/CityActivity.java
+++ app/src/main/java/com/lin/atm/CityActivity.java
@@ -1,5 +1,6 @@
 package com.lin.atm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.Spinner;
 
 public class CityActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     ListView city;
+    public static final int  GO_KEELUNG = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +19,26 @@ public class CityActivity extends AppCompatActivity implements AdapterView.OnIte
         city = (ListView) findViewById(R.id.city);
         String[] data ={"基隆市","新北市","台北市"};
         ArrayAdapter adapter =new ArrayAdapter(this,android.R.layout.simple_list_item_1,data);
+        city.setAdapter(adapter);
         city.setOnItemClickListener(this);
     }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        String[] data = null;
+        if(position == 0){
+            Intent arealayout = new Intent(this,AreaActivity.class);
+            startActivityForResult(arealayout,GO_KEELUNG);
+            data = new String[]{"暖暖區","八堵區","中正區"};
+        }
+        if(position == 1){
+            data = new String[]{"永和區","新莊區","板橋區"};
+        }
+        if(position == 2){
+            data = new String[]{"大安區","士林區","信義區"};
+
+        }
+        ArrayAdapter adapter = new ArrayAdapter(CityActivity.this,android.R.layout.simple_list_item_1,data);
 
     }
 }
